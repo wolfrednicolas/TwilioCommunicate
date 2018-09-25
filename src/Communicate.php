1be0cc6fff71087892f34361556a085cf1e764a0
Wolfred Montilla <wolfrednicolas@gmail.com>
2018-09-25 18:16:13 -0400

    now is available to create a new subaccount and instance all the info, deppends on the project its possible to save the APNS and the FCM

--- src/Communicate.php
+++ src/Communicate.php
@@ -3,14 +3,16 @@ namespace Wolfred\Twilio;
 
 use Twilio\Rest\Client;
 use Twilio\Exceptions\TwilioException;
+use URL;
 
 class Communicate
 {
 	protected $config;
 
 	public function __construct(){
-		$r = new Config();
-		$this->config = $r->config;
+		$config = new Config();
+		$this->config = $config->config;
+		$this->conf = $config;
 	}
 
 	public function hasInstance($bool = true)  
@@ -18,11 +20,96 @@ class Communicate
 		return $bool;  
 	}
 
-	public function createSubaccount($name)
+	public function createSubaccount($account_id, $FCM=NULL, $APNS=NULL, $VOIP=FALSE)
 	{
-		$twilio  = new Client($this->config['accountSid'], $this->config['authToken']);
-		$account = $twilio->api->v2010->accounts
+		$name = "{$this->config['name']}_{$this->config['env']}_{$account_id}";
+		$twilio = new Client($this->config['accountSid'], $this->config['authToken']);
+		$sub_account = $twilio->api->v2010->accounts
                               ->create(array("friendlyName" => $name));
+                              \Log::info('este es el nombre', (array)$name);
+        $client = new Client($sub_account->sid, $sub_account->authToken);
+
+        $new_key = $client->newKeys
+                 ->create(array('friendlyName' => $account_id));
+
+        $array = [
+        	'sid'   => $sub_account->sid,
+        	'token' => $sub_account->authToken,
+        	'api_key' => $new_key->sid,
+        	'api_secret' => $new_key->secret
+        ];
+
+        //credentials for firebase
+        if($FCM !== NULL){
+           \Log::info('entro fcm');
+           /* $FCM_credential = $client->notify->v1->credentials->create("fcm",
+                                          array(
+                                              "friendlyName" => "FCMCredentialFor{$account->id}",
+                                              "secret" => getenv('FCM_SECRET')
+                                         ) 
+                                 );    	
+	        $array_merge = [
+	        	'FCM_push_credential' => $FCM_credential->sid
+	        ];
+	        $array = array_merge($array,$array_merge);*/
+        }
+
+        //APNS for IOS notification, se deberia configurar los certificados
+        if($APNS !== NULL){
+	        \Log::info('entro APNS');
+	        /*$env = getenv('APP_ENV');
+	        $credentials = \Config::get('services.APNS.'.$env);
+	        $APN_credential = $client->notify->v1->credentials
+	                                ->create("apn", // type
+	                                         array(
+	                                             "certificate" => $credentials['certificate'],
+	                                             "friendlyName" => "APNCredentialFor{$account->id}",
+	                                             "privateKey" => $credentials['privateKey'],
+	                                             "sandbox" => (boolean) $credentials['sandbox']
+	                                         )
+	                                );
+	        $array_merge = [
+	        	'APN_push_credential' => $APN_credential->sid
+	        ];
+	        $array = array_merge($array,$array_merge);  */	
+        }
+
+        $application = $client->api->applications->create('Main', [
+            'VoiceUrl'             => "{$this->config['url']}/{$account_id}/infrastructure/phone/calls/initial",
+            'VoiceMethod'          => 'POST',
+
+            'VoiceFallbackUrl'     => "{$this->config['url']}/{$account_id}/infrastructure/phone/calls/initial",
+            'VoiceFallbackMethod'  => 'POST',
+
+            'StatusCallback'       => "{$this->config['url']}/{$account_id}/infrastructure/phone/calls/status",
+            'StatusCallbackMethod' => 'POST',
+
+            'SmsUrl'               => "{$this->config['url']}/infrastructure/tracking/phone/incoming?account_id={$account_id}&message_type=sms",
+            'SmsMethod'            => 'POST',
+
+            'SmsFallbackUrl'       => "{$this->config['url']}/infrastructure/tracking/phone/incoming?account_id={$account_id}&message_type=sms",
+            'SmsFallbackMethod'    => 'POST'
+        ]);
+
+	    $array_merge = [
+    		'application_sid' => $application->sid
+	    ];
+	    $array = array_merge($array,$array_merge); 
+
+        //convert automaticly in VOIP the numbers, at this moment are not going to be used
+        if($VOIP === true){
+	        $voip_car = $client->applications->create('VoIP', [
+	            'VoiceUrl'             => "{$this->config['url']}/twilio-mobile/voice/{account_id}",
+	            'VoiceMethod'          => 'POST'
+	        ]);
+		    
+		    $array_merge = [
+	    		'voip_sid' => $voip_car->sid
+		    ];
+	    	$array = array_merge($array,$array_merge); 	
+        }
+
+        $this->conf->setInfrastructureConfig('twilio', 'authentication', $array, $account_id);
 	}
 
 	public function getConfig(){
--- src/Config.php
+++ src/Config.php
@@ -1,13 +1,44 @@
 <?php
 namespace Wolfred\Twilio;
 
+use DB;
+
 class Config
 {
 	public $config;
 
 	public function __construct(){
-		$this->config = [ "accountSid" 	=> getenv("accountsid"),
-						  "authToken" 	=> getenv("authtoken")];
+		$this->config = [ "accountSid" 	=> getenv("ACCOUNTSID"),
+						  "authToken" 	=> getenv("AUTHTOKEN"),
+						  'env' 		=> getenv("APP_ENV"),
+						  'name' 		=> getenv("APP_NAME"),
+						  'url' 		=> getenv("APP_URL")
+						];
+	}
+
+	public function setInfrastructureConfig($service, $field, $data, $account_id=NULL)
+	{
+		if (is_array($data))
+            $data = json_encode($data);
+
+        $query = [
+            'account_id' => $account_id,
+            'service'    => $service,
+            'field'      => $field,
+            'data'       => $data,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ];
+
+        DB::table('infrastructure_config')
+            ->where('account_id', $account_id)
+            ->where('service', $service)
+            ->delete();
+
+        DB::table('infrastructure_config')->insert($query);
+        
+        return true;
+
 	}
 
 }
